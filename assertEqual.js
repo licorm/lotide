c5383bb9524fd5281603f437133ea0565a3f87bf
Lisa Cormier <lcormier5@hotmail.com>
2021-08-16 20:42:38 +0000

    adding template literal

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('🥳🥳💚Assertion Passed: [' + actual + '] === [' + expected + ']');
+    console.log(`🥳🥳💚Assertion Passed: [${actual}] === [${expected}]`);
   } else {
-    console.log('😰😰💔Assertion Failed: [' + actual + '] !== [' + expected + ']');
+    console.log(`😰😰💔Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
 
