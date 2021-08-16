69e08323c743f67f77fccb385987877306b13597
Lisa Cormier <lcormier5@hotmail.com>
2021-08-16 20:35:08 +0000

    creating new assertion function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🥳🥳💚Assertion Passed: [' + actual + '] === [' + expected + ']');
+  } else {
+    console.log('😰😰💔Assertion Failed: [' + actual + '] !== [' + expected + ']');
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
