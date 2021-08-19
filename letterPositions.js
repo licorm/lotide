0179e32555f47b4d64832e3a9626a2408d4772d9
Lisa Cormier <lcormier5@hotmail.com>
2021-08-19 00:16:09 +0000

    linted file, still needs functions to be separated in future

--- letterPositions.js
+++ letterPositions.js
@@ -31,12 +31,12 @@ const letterPositions = function(sentence) {
   }
    
   for (let i = 0; i < noSpace.length; i++) {
-    const letter = noSpace[i]
-      if(results[letter]) {
-        results[letter].push(i);
-      } else {
-        results[letter] = [i];
-      }
+    const letter = noSpace[i];
+    if (results[letter]) {
+      results[letter].push(i);
+    } else {
+      results[letter] = [i];
+    }
   }
 
   console.log(results);
