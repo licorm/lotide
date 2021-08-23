03de6c762fb70173c109a5df838b33420c4f0783
Lisa Cormier <lcormier5@hotmail.com>
2021-08-23 19:54:12 +0000

    refracted assertArraysEq and added test for assertArraysEq

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,7 @@
-// Function for asserting if two arrays are identical
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i] || typeof(array1[i]) !== typeof(array2[i])) {
-      return false;
-    }
-  }
-  return true;
-};
+// Import eqArrays
+const eqArrays = require('./eqArrays'); 
 
+//function for asserting arrays equal
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === true) {
     console.log(`🥳🥳💚Assertion Passed: [${arr1}] === [${arr2}]`);
@@ -19,5 +10,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+//export function
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+//import function
+const assertArraysEqual = require('../assertArraysEqual');
+
+//test code
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); //false
