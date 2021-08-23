780fe4d3a620979eaf163ce830331ab47a0f83f8
Lisa Cormier <lcormier5@hotmail.com>
2021-08-23 20:31:03 +0000

    added mocha/chai to tailTest file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,17 @@
 //import modules
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 //test code
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns ['lighthouse', 'labs'] for ['yo yo', 'lighthouse', 'labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
+
