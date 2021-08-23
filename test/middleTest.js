8d8ae48848aa2fa83f09a77b2278172c2dea1510
Lisa Cormier <lcormier5@hotmail.com>
2021-08-23 20:37:33 +0000

    added mocha/chai to middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,20 @@
 //import functions
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 //test code
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 4]), [2]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1]), []);
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [2] for [1, 2, 4]", () => {
+    assert.deepEqual(middle([1, 2, 4]), [2]);
+  });
+  it("returns [] for [1, 2,]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
+
