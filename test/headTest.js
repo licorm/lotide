021549b8caa25b9ade6790e95f667417b1abfc62
Lisa Cormier <lcormier5@hotmail.com>
2021-08-23 20:20:08 +0000

    added mocha/chai testing to our headTest file

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
 //import head
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
 //test code
-assertEqual(head([]), 5);
-assertEqual(head([1]), 1);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns undefined when given an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns '1' for ['1']", () => {
+    assert.strictEqual(head(['1']), '1');
+  });
+});
+
+//test code
+//assertEqual(head([]), 5);
+//assertEqual(head([1]), 1);
