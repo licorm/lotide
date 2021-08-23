7cc75ed804abfeef3ea366b946c5b0c011860ccc
Lisa Cormier <lcormier5@hotmail.com>
2021-08-23 20:44:50 +0000

    added single file to export our three mocha/chai functions (head, middle, tail)

---
+++ index.js
@@ -0,0 +1,11 @@
+//importing our functions
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+
+//export our functions as objects
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle
+};
