570620b855efe0b65e05aa96c72a1adb4278a6b7
Lisa Cormier <lcormier5@hotmail.com>
2021-10-02 21:43:50 +0000

    adding recursive strategy to eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -3,21 +3,33 @@ const eqArrays = require('./eqArrays');
 
 // function to check if two seperate objects are equal
 const eqObjects = function(object1, object2) {
+
   //make sure objects have same number of keys
-  
   let numOfKeys1 = Object.keys(object1);
   let numOfKeys2 = Object.keys(object2);
   if (numOfKeys1.length !== numOfKeys2.length) {
+    console.log("keys don't equal")
     return false;
   }
 
   //make sure values of each key are the same
   for (const value in object1) {
+    
     if (Array.isArray(object1[value]) && Array.isArray(object2[value])) {
       if (eqArrays(object1[value], object2[value]) !== true) {
+        
         return false;
       }
-    } else if (object1[value] !== object2[value]) {
+    } 
+
+    //if the value is an object, call function
+    if (Object.prototype.toString.call(object1[value]) === '[object Object]') {
+      
+      return eqObjects(object1[value], object2[value])
+    }
+
+    if (object1[value] !== object2[value]) {
+     
       return false;
     }
   }
